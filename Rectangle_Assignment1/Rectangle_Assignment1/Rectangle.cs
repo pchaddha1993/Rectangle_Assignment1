7defe03d02e76cbbaf1872f39771989e54a23ad4
webil <webil@MSI>
2021-06-15 15:20:06 +0530

    committing Rectangle class

---
+++ Rectangle_Assignment1/Rectangle_Assignment1/Rectangle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rectangle_Assignment1
+{
+    class Rectangle
+    {
+        private int Rec_Length, Rec_Width;
+
+        public Rectangle()
+        {
+            Rec_Length = 1;
+            Rec_Width = 1;
+        }
+
+        public Rectangle(int Rec_Length, int Rec_Width)
+        {
+            this.Rec_Length = Rec_Length;
+            this.Rec_Width = Rec_Width;
+        }
+
+        public int GetLength()
+        {
+            return Rec_Length;
+        }
+        public int SetLength(int length)
+        {
+            this.Rec_Length = Rec_Length;
+            return this.Rec_Length;
+
+        }
+        public int GetWidth()
+        {
+            return Rec_Width;
+        }
+        public int SetWidth(int width)
+        {
+            this.Rec_Width = Rec_Width;
+            return this.Rec_Width;
+        }
+        public int GetPerimeter()
+        {
+            return Rec_Length * Rec_Width;
+        }
+        public int GetArea()
+        {
+            return 2 * (Rec_Length + Rec_Width);
+        }
+
+    }
+}
